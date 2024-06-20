f3cf0aefd2ae12c91a39b6cee3742a5ba3e5052e
John <jonathan.ulmer@ulmail.de>
2024-06-20 15:23:20 +0200

    flake

--- flake.nix
+++ flake.nix
@@ -10,9 +10,9 @@
   };
 
   inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     flake-utils.url = "github:numtide/flake-utils";
     flake-utils.inputs.nixpkgs.follows = "nixpkgs";
-    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
 
     devshell.url = "github:numtide/devshell";
     devshell.inputs.nixpkgs.follows = "nixpkgs";
@@ -56,6 +56,71 @@
           # default = project;
         };
         devShells.default = pkgs.devshell.mkShell {
+
+          packages = with pkgs; [
+            cudatoolkit
+            cudaPackages.cudnn
+            git
+            gitRepo
+            pdf2svg
+            gnupg
+            autoconf
+            curl
+            procps
+            gnumake
+            util-linux
+            m4
+            gperf
+            unzip
+            libGLU
+            libGL
+            xorg.libXi
+            xorg.libXmu
+            freeglut
+            xorg.libXext
+            xorg.libX11
+            xorg.libXv
+            xorg.libXrandr
+            zlib
+            ncurses5
+            stdenv.cc
+            binutils
+          ];
+          env = [
+            {
+              name = "NIX_LD_LIBRARY_PATH";
+              value = pkgs.lib.makeLibraryPath [
+                pkgs.stdenv.cc.cc
+                # ...
+              ];
+            }
+            {
+              name = "NIX_LD";
+              value = "${pkgs.glibc}/lib/ld-linux-x86-64.so.2";
+            }
+            {
+              name = "CUDA_PATH";
+              value = "${pkgs.cudatoolkit}";
+            }
+            {
+              name = "LD_LIBRARY_PATH";
+              value =
+                "${pkgs.linuxPackages.nvidia_x11_production}/lib:${pkgs.ncurses5}/lib";
+            }
+            {
+              name = "EXTRA_LDFLAGS";
+              value =
+                "-L/lib -L${pkgs.linuxPackages.nvidia_x11_production}/lib";
+            }
+            {
+              name = "EXTRA_CCFLAGS";
+              value = "-I/usr/include";
+            }
+            {
+              name = "JULIA_NUM_THREADS";
+              value = "auto";
+            }
+          ];
           imports = [
             # you can keep either one of them devshellProfiles.packages or julia-wrapped
             # inputs.julia2nix.${pkgs.system}.julia2nix.devshellProfiles.packages
