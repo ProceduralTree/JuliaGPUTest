42bdc589d6ca117e34c1228427749649f2a930c4
Jonathan Ulmer <jonathan.ulmer@ulmail.de>
2024-09-18 10:41:48 +0200

    dynamic bd

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,6 +1,6 @@
 ((julia-mode . ((julia-snail-port . 10050)
                 (julia-snail-repl-buffer . "*julia Venus*")
-                (julia-snail-executable . "nix develop --impure --command julia")))
+                (julia-snail-executable . "julia")))
 
 (org-mode . ((julia-snail-port . 10060)
                 (julia-snail-repl-buffer . "*julia Org*")
